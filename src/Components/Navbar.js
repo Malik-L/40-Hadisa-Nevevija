6b61be2f1a24c820f895e9531582b166ce6d7564
Malik Lemes <netixntx@gmail.com>
2021-03-07 15:00:52 +0100

    Napravio kostur aplikacije

---
+++ src/Components/Navbar.js
@@ -0,0 +1,71 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+    useLocation,
+  NavLink
+} from "react-router-dom";
+
+
+const Navbar = () => (
+    <div className="scroll-bar-menu">
+        <ul>
+            <Router>
+                <NavLink to="#">Hadis br.1</NavLink>
+           </Router>
+            <li>Hadis br.2</li>
+            <li>Hadis br.3</li>
+            <li>Hadis br.4</li>
+            <li>Hadis br.5</li>
+            <li>Hadis br.6</li>
+            <li>Hadis br.7</li>
+            <li>Hadis br.8</li>
+            <li>Hadis br.9</li>
+            <li>Hadis br.10</li>
+            <li>Hadis br.11</li>
+            <li>Hadis br.12</li>
+            <li>Hadis br.13</li>
+            <li>Hadis br.14</li>
+            <li>Hadis br.15</li>
+            <li>Hadis br.16</li>
+            <li>Hadis br.17</li>
+            <li>Hadis br.18</li>
+            <li>Hadis br.19</li>
+            <li>Hadis br.20</li>
+            <li>Hadis br.21</li>
+            <li>Hadis br.22</li>
+            <li>Hadis br.23</li>
+            <li>Hadis br.24</li>
+            <li>Hadis br.25</li>
+            <li>Hadis br.26</li>
+            <li>Hadis br.27</li>
+            <li>Hadis br.28</li>
+            <li>Hadis br.29</li>
+            <li>Hadis br.30</li>
+            <li>Hadis br.31</li>
+            <li>Hadis br.32</li>
+            <li>Hadis br.33</li>
+            <li>Hadis br.34</li>
+            <li>Hadis br.35</li>
+            <li>Hadis br.36</li>
+            <li>Hadis br.37</li>
+            <li>Hadis br.38</li>
+            <li>Hadis br.39</li>
+            <li>Hadis br.40</li>
+            <li>Hadis br.41</li>
+            <li>Hadis br.42</li>
+            <li>Hadis br.43</li>
+            <li>Hadis br.44</li>
+            <li>Hadis br.45</li>
+            <li>Hadis br.46</li>
+            <li>Hadis br.47</li>
+            <li>Hadis br.48</li>
+            <li>Hadis br.49</li>
+            <li>Hadis br.50</li>
+       </ul>
+  </div>
+);
+
+export default Navbar;
